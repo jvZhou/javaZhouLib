950f7e6ba26dc4551db80db42e78736235dc39a5
JavaZhou <hasjavaeezhou@163.com>
2021-09-02 10:46:28 +0800

    1.0

---
+++ src/main/java/com/java/zhou/PruduceMeApplication.java
@@ -0,0 +1,11 @@
+package com.java.zhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruduceMeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PruduceMeApplication.class, args);
+    }
+}
